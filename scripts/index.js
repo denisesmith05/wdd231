9bfe5dfa417b88f7eab354ee4fad801362cbafa4
Denise <denise.sunshine@icloud.com>
2024-11-23 21:27:29 -0800

    added hamburger nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
 document.getElementById('lastUpdated').textContent = new Date().toLocaleString();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.navbar-toggler');
+    const navLinks = document.querySelector('.navigation ul');
+
+    toggleButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active'); // Toggles visibility of navigation links
+    });
+});
